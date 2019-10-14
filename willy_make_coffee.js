e53be73463f8fe6984b32b3b672ccfdd232ca4e8
S de V <smavrij@hotmail.com>
2019-10-14 11:42:05 +0200

    takecarfromwilly

--- willy_make_coffee.js
+++ willy_make_coffee.js
@@ -1,19 +1,18 @@
+create class Human {
+	constructor(milk, sugar){
+		this.milk = milk;
+		this.sugar = sugar;
+	}
 
 
- blalbalblalbalb
- 
-
+}
 
 var willy = new Human("William Loosman", {age: 26, IQ : "over 2000"});
-willy.makeCoffee("Milk and Sugar");
+willy.makeCoffee({ milk: 0.2, sugar: 1});
+willy.makeCoffee({milk : 0, sugar : 0});
+
+var willemijn = new Human("Willemijn ???", {null : null});
+willemijn.takeCarFrom(willy);
 
 
 
-
-
-
-willy.makecoffee("now")
-
-sdfdfd
-
-conflict
