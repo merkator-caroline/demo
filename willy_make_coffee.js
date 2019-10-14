ef572a73a7a4ec92862200bfc7f0594f441247be
S de V <smavrij@hotmail.com>
2019-10-14 12:09:38 +0200

    lala

--- willy_make_coffee.js
+++ willy_make_coffee.js
@@ -1,9 +1,12 @@
-create class Human {
-	constructor(milk, sugar){
-		this.milk = milk;
-		this.sugar = sugar;
-	}
+export class Human {
+	
+	name;
+	options;
 
+	constructor(name, options){
+		this.name = name;
+		this.options = options;
+	}
 
 }
 
@@ -11,7 +14,7 @@ var willy = new Human("William Loosman", {age: 26, IQ : "over 2000"});
 willy.makeCoffee({ milk: 0.2, sugar: 1});
 willy.makeCoffee({milk : 0, sugar : 0});
 
-var willemijn = new Human("Willemijn ???", {null : null});
+var willemijn = new Human("Willemijn ???", {age : 18});
 willemijn.takeCarFrom(willy);
 
 
